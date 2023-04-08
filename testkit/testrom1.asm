571c4f3fe248ed4182911bbfe8fd68a59aa0f8b5
Julian Brown <julian.brown@the-lair.info>
2023-04-08 14:25:58 +0100

    refine test1

--- testkit/testrom1.asm
+++ testkit/testrom1.asm
@@ -1,19 +1,24 @@
             org     $8000
 
 * Verify operation of cpu
-rst_entry   lds     #$7FFF      initialise system stack
-            ldu     #$6FFF      initialise user stack
+rst_entry   lds     #$7FFF      ;initialise system stack
+            ldu     #$6FFF      ;initialise user stack
+            lda     #$00
+            tfr     A,DP        ;set DP to 0
+
 test        lda     #$55
             ldx     #$6000
-loop1       sta     $0000, X
+
+loop1       sta     $0000, X    ;fill $0000 to $5FFF
             leax    -1, X
             bne     loop1
             lda     #$AA
             ldx     #$6000
-loop2       sta     $0000, X
+
+loop2       sta     $0000, X    ;fill $0000 to $5FFF
             leax    -1, X
             bne     loop2
-            bra     test
+            bra     test        ;repeat
 nmi_entry   rti
 swi_entry   rti
 irq_entry   rti

777474ad0eea73138708dbcf1a16116ae0e830fe
julian <julian@the-lair.info>
2023-04-08 21:44:29 +0100

    add memory handling

--- testkit/testrom1.asm
+++ testkit/testrom1.asm
@@ -1,4 +1,4 @@
-            org     $8000
+            org     $C000
 
 * Verify operation of cpu
 rst_entry   lds     #$7FFF      ;initialise system stack
@@ -19,6 +19,37 @@ loop2       sta     $0000, X    ;fill $0000 to $5FFF
             leax    -1, X
             bne     loop2
             bra     test        ;repeat
+
+setpage     sta     $FF25       ;set memory page
+            rst_entry
+
+pagemode    cmpa    #$00        ;enable/disable paging
+            bne     enablepage
+            sta     $FF24       ;disable
+            rts
+enablepage  pshu    A           ;enable
+            lda     #$01
+            sta     $FF24
+            pulu    A
+            rts
+
+shadow      pshu    A,B,X,Y     ;shadow rom into high mem
+* copy rom from C000 to 4000
+            ldx     #$C000
+            ldy     #$4000
+            jsr     blockcopy
+* enable page mode
+            lda     #$01
+            jsr     pagemode
+* copy rom from 4000 back to C000
+            ldx     #$4000
+            ldy     #$C000
+            jsr     blockcopy
+            pulu    A,B,X,Y
+            rts
+* copy block from X to Y
+blockcopy   rts
+
 nmi_entry   rti
 swi_entry   rti
 irq_entry   rti
